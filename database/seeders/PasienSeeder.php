6e8a41b56e91ebc935e2c9568b4a9542a51e54dc
Brian <brian.purnama@mhs.unsoed.ac.id>
2024-08-09 07:58:39 +0700

    feat: Update PasienSeeder to include a default value for 'nomor_rm'

--- database/seeders/PasienSeeder.php
+++ database/seeders/PasienSeeder.php
@@ -27,7 +27,7 @@ class PasienSeeder extends Seeder
                 'nomor_kartu' => str_pad(mt_rand(0, 99999999), 8, '0', STR_PAD_LEFT),
             ],
             [
-                'nomor_rm' => '',
+                'nomor_rm' => '123456',
                 'nama_pasien' => 'Budi Santoso',
                 'tempat_lahir' => 'Bandung',
                 'tanggal_lahir' => '2024-11-30',
